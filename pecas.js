f7a41778dfcae1fa8608f0a9b22c402cffe40af3
eduardoruizmouro <ruizmouroeduardo@gmail.com>
2021-12-28 09:14:46 -0300

    Sistema de cadastro de peças finalizado

---
+++ pecas.js
@@ -0,0 +1,26 @@
+let pesoPeca = 150
+
+if(pesoPeca > 100) {
+    console.log("Peso maior que 100 gramas,logo podemos cadastrar a peca !!! ")
+} else {                                                                                                                           
+     console.log("Peso insuficiente,não é possivel cadastrar")      
+}
+  
+ let numeroPecas=11
+
+
+ if(numeroPecas < 10){
+     console.log("Ainda há espaco na caixa ,podemos cadastrar.")
+ } else{
+     console.log("Nao há espaco suficiente na caixa.")
+ }
+
+ let nomePeca = "Disco de freio"
+
+ console.log("O comprimento do nome da peça é:",nomePeca.length)
+
+ if(nomePeca.length<3){
+     console.log("nome da peça muito curto,não é possivel cadastrar")
+ } else { console.log ("nome da peça é possível cadastrar")
+
+ } 
